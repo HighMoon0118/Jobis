b3854c80f5acc0a1c39d81581b67c9beada199e4
hyunseungbb <happyman1104@naver.com>
2021-10-25 17:00:30 +0900

    feat: 로그인 및 회원가입 페이지 및 ViewModel 생성

---
+++ app/src/main/java/com/example/jobis/presentation/login/UserActivity.kt
@@ -0,0 +1,33 @@
+package com.example.jobis.presentation.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.jobis.R
+import com.example.jobis.databinding.ActivityUserBinding
+import com.example.jobis.presentation.login.ui.login.LoginFragment
+import com.example.jobis.presentation.signup.ui.login.SignupFragment
+
+class UserActivity : AppCompatActivity() {
+
+    val binding by lazy { ActivityUserBinding.inflate(layoutInflater)}
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.add(R.id.userFrame, LoginFragment())
+            .commit()
+    }
+
+    fun goSignUp() {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.userFrame, SignupFragment())
+            .commit()
+    }
+
+    fun goLogin() {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.userFrame, LoginFragment())
+            .commit()
+    }
+}
