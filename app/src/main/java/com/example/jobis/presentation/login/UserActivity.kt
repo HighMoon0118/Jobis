930be65e495b9b24b63409a63b7c1f14d06d01b1
hyunseungbb <happyman1104@naver.com>
2021-10-27 15:17:13 +0900

    feat: login 기능 구현

--- app/src/main/java/com/example/jobis/presentation/login/UserActivity.kt
+++ app/src/main/java/com/example/jobis/presentation/login/UserActivity.kt
@@ -1,9 +1,13 @@
 package com.example.jobis.presentation.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import com.example.jobis.R
 import com.example.jobis.databinding.ActivityUserBinding
+import com.example.jobis.presentation.MainActivity
 import com.example.jobis.presentation.login.ui.login.LoginFragment
 import com.example.jobis.presentation.signup.ui.signup.SignupFragment
 
@@ -29,5 +33,19 @@ class UserActivity : AppCompatActivity() {
         val transaction = supportFragmentManager.beginTransaction()
         transaction.replace(R.id.userFrame, LoginFragment())
             .commit()
+    }
+
+    fun goMain() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    fun loadingOff() {
+        binding.loading.visibility = View.GONE
+    }
+    fun loadingOn() {
+        Log.d("test", "로딩 온!!")
+        binding.loading.visibility = View.VISIBLE
     }
 }
