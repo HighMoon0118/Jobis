a302fed9868a8b85d0b406af9af7931d8612ad33
hyunseungbb <happyman1104@naver.com>
2021-11-11 11:53:41 +0900

    feat: 커뮤니티  게시글 검색 기능 완료

--- app/src/main/java/com/ssafy/jobis/presentation/community/search/ui/CommunitySearchViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.ssafy.jobis.presentation.community.search.ui
-
-import androidx.lifecycle.ViewModel
-
-class CommunitySearchViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
