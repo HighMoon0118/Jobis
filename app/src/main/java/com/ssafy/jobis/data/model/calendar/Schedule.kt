dc232043f9fad8c485e049b1e49c7dcbe5d0ac03
Seongho <skk7541@gmail.com>
2021-11-11 11:34:03 +0900

    feat: 알람 구현중

--- app/src/main/java/com/ssafy/jobis/data/model/calendar/Schedule.kt
+++ app/src/main/java/com/ssafy/jobis/data/model/calendar/Schedule.kt
@@ -3,24 +3,24 @@ package com.ssafy.jobis.data.model.calendar
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.util.*
 
 @Entity
 data class Schedule (
     @ColumnInfo var title: String,
     @ColumnInfo var content: String,
+    // 날짜
     @ColumnInfo var year: Int,
     @ColumnInfo var month: Int,
-    @ColumnInfo var day: Int
+    @ColumnInfo var day: Int,
+    // 일정 시작 시간
+    @ColumnInfo var start_time: String,
+    @ColumnInfo var end_time: String,
+    // 일정이 스터디에서 만든 일정이라면 studyId를 추가
+    @ColumnInfo var studyId: Int,
+    // 일정이 주간일정이라면 groupId를 추가
+    @ColumnInfo var groupId: Int,
+    @ColumnInfo var companyName: String
     ) {
     @PrimaryKey(autoGenerate = true) var id: Int = 0
 }
-
-
-//@Entity
-//data class Schedule (
-//    var title: String,
-//    var content: String,
-//    var date: String
-//) {
-//    @PrimaryKey(autoGenerate = true) var id:Int = 0
-//}
