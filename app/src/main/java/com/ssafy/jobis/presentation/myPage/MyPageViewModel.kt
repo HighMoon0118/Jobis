5c4ae8f50b2e82ffc580f34e85ad21f74f52c2c1
hyunseungbb <happyman1104@naver.com>
2021-11-12 17:11:43 +0900

    feat: 마이페이지 커뮤니티 조회  & 커뮤니티 카테고리분류 기능 추가

--- app/src/main/java/com/ssafy/jobis/presentation/myPage/MyPageViewModel.kt
+++ app/src/main/java/com/ssafy/jobis/presentation/myPage/MyPageViewModel.kt
@@ -3,6 +3,7 @@ package com.ssafy.jobis.presentation.myPage
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.ssafy.jobis.data.model.community.Comment
 import com.ssafy.jobis.data.repository.MyPageRepository
 import com.ssafy.jobis.data.response.PostResponseList
 import kotlinx.coroutines.CoroutineScope
@@ -17,24 +18,27 @@ class MyPageViewModel(private val myPageRepository: MyPageRepository): ViewModel
     private val _myPostList = MutableLiveData<PostResponseList>()
     val myPostList: LiveData<PostResponseList> = _myPostList
 
-    private val _myCommentList = MutableLiveData<PostResponseList>()
-    val myCommentList: LiveData<PostResponseList> = _myCommentList
+    private val _myCommentList = MutableLiveData<MutableList<Comment>>()
+    val myCommentList: LiveData<MutableList<Comment>> = _myCommentList
 
     fun loadMyLikeList(uid: String) {
         CoroutineScope(Dispatchers.Main).launch {
             val res = myPageRepository.loadMyLikePosts(uid)
+            _myLikeList.value = res
         }
     }
 
     fun loadMyPostList(uid: String) {
         CoroutineScope(Dispatchers.Main).launch {
             val res = myPageRepository.loadMyPosts(uid)
+            _myPostList.value = res
         }
     }
 
     fun loadMyCommentList(uid: String) {
         CoroutineScope(Dispatchers.Main).launch {
             val res = myPageRepository.loadMyCommentPosts(uid)
+            _myCommentList.value = res
         }
     }
 }
