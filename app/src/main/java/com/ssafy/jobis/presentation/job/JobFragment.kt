2706b3d028b7dc8b2937812d626c4996dd86a55b
hyunseungbb <happyman1104@naver.com>
2021-11-15 15:47:51 +0900

    feat: 채용공고 등록기능 완성

--- app/src/main/java/com/ssafy/jobis/presentation/job/JobFragment.kt
+++ app/src/main/java/com/ssafy/jobis/presentation/job/JobFragment.kt
@@ -1,12 +1,23 @@
 package com.ssafy.jobis.presentation.job
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.ssafy.jobis.data.model.calendar.CalendarDatabase
+import com.ssafy.jobis.data.model.calendar.Schedule
+import com.ssafy.jobis.data.response.JobResponse
 import com.ssafy.jobis.databinding.FragmentJobBinding
+import com.ssafy.jobis.presentation.MainActivity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class JobFragment: Fragment() {
 
@@ -27,12 +38,61 @@ class JobFragment: Fragment() {
         super.onViewCreated(view, savedInstanceState)
         jobViewModel = ViewModelProvider(this, JobViewModelFactory())
             .get(JobViewModel::class.java)
-
-
+        jobViewModel.jobList.observe(viewLifecycleOwner,
+            Observer { jobList ->
+                jobList ?:return@Observer
+                updateJobList(jobList)
+            })
+        jobViewModel.scheduleResult.observe(viewLifecycleOwner,
+            Observer { scheduleResult ->
+                scheduleResult ?:return@Observer
+                if (scheduleResult) {
+                    Toast.makeText(context, "공고가 달력에 등록되었습니다.", Toast.LENGTH_LONG).show()
+                } else {
+                    Toast.makeText(context, "등록 실패.", Toast.LENGTH_LONG).show()
+                }
+            })
+        jobViewModel.loadJobList()
     }
 
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
+    }
+
+    private fun updateJobList(jobList: MutableList<JobResponse>) {
+        Log.d("test", "jobList: ${jobList}")
+        val adapter = JobAdapter()
+        adapter.listData = jobList
+        adapter.setOnItemClickListener(object: JobAdapter.OnItemClickListener{
+            // 각 채용공고 클릭 시 동작하는 함수
+            // 여기서 알림설정 코드 작성
+            override fun onItemClick(v: View, job: JobResponse, post: Int) {
+                var newSchedule = Schedule(
+                    title=job.title,
+                    content=job.content,
+                    year=job.year.toInt(),
+                    month=job.month.toInt()-1,
+                    day=job.day.toInt(),
+                    start_time = "",
+                    end_time = job.end_time,
+                    studyId = 0,
+                    groupId = 0,
+                    companyName = job.companyName
+                )
+                var db = CalendarDatabase.getInstance(context)
+                CoroutineScope(Dispatchers.IO).launch {
+                    db?.calendarDao()?.insert(newSchedule)
+                }
+                
+                // 여기에서 알림설정 코드 작성해주면 될듯
+                
+            }
+        })
+        binding.jobRecyclerView.adapter = adapter
+        binding.jobRecyclerView.layoutManager = StaggeredGridLayoutManager(
+            2,
+            StaggeredGridLayoutManager.VERTICAL
+        )
     }
 }
--- app/src/main/java/com/ssafy/jobis/presentation/job/JobViewModel.kt
+++ app/src/main/java/com/ssafy/jobis/presentation/job/JobViewModel.kt
@@ -1,8 +1,11 @@
 package com.ssafy.jobis.presentation.job
 
+import android.content.Context
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.ssafy.jobis.data.model.calendar.Schedule
 import com.ssafy.jobis.data.repository.JobRepository
 import com.ssafy.jobis.data.response.JobResponse
 import kotlinx.coroutines.CoroutineScope
@@ -11,13 +14,23 @@ import kotlinx.coroutines.launch
 
 class JobViewModel(private val jobRepository: JobRepository): ViewModel() {
 
-    private val _jobList = MutableLiveData<JobResponse>()
-    val jobList: LiveData<JobResponse> = _jobList
+    private val _jobList = MutableLiveData<MutableList<JobResponse>>()
+    val jobList: LiveData<MutableList<JobResponse>> = _jobList
+    private val _scheduleResult = MutableLiveData<Boolean>()
+    val scheduleResult: LiveData<Boolean> = _scheduleResult
+
 
     fun loadJobList() {
         CoroutineScope(Dispatchers.Main).launch {
             val res = jobRepository.loadJobList()
             _jobList.value = res
+            Log.d("test", "${res}")
+        }
+    }
+
+    fun insertSchedule(scehdule: Schedule, context: Context) {
+        CoroutineScope(Dispatchers.Main).launch {
+            _scheduleResult.value = jobRepository.insertSchedule(scehdule, context)
         }
     }
 }
