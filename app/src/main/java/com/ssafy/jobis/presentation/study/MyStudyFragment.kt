a2059b0c6fe2cfadcc1a6abf289640b146a294be
문학일 <mhi0118@naver.com>
2021-11-16 13:23:11 +0900

    ADD: 스터디 뷰모델 연결

--- app/src/main/java/com/ssafy/jobis/presentation/study/MyStudyFragment.kt
+++ app/src/main/java/com/ssafy/jobis/presentation/study/MyStudyFragment.kt
@@ -1,76 +1,44 @@
 package com.ssafy.jobis.presentation.study
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
-import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.LiveData
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.ssafy.jobis.data.model.study.Study
-import com.ssafy.jobis.data.model.study.StudyDatabase
+import androidx.lifecycle.ViewModelProvider
 import com.ssafy.jobis.databinding.FragmentMyStudyBinding
-import com.ssafy.jobis.presentation.chat.ChatActivity
 import com.ssafy.jobis.presentation.study.adapter.MyStudyAdapter
-import com.ssafy.jobis.presentation.study.adapter.TmpAdapter
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
-class MyStudyFragment: Fragment() {
+class MyStudyFragment(val myContext: Context): Fragment() {
 
     private var _binding: FragmentMyStudyBinding? = null
     private val binding get() = _binding!!
 
-//    private lateinit var studyList:LiveData<List<Study>>
+    private lateinit var viewModel: StudyViewModel
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,savedInstanceState: Bundle?): View? {
 
         _binding = FragmentMyStudyBinding.inflate(inflater, container, false)
 
+        activity?.run {
+            viewModel = ViewModelProvider(this, ViewModelProvider.AndroidViewModelFactory(this.application)).get(StudyViewModel::class.java)
+        }
+
+        val studyAdapter = MyStudyAdapter(myContext)
+        binding.rvMyStudy.adapter = studyAdapter
+
+        viewModel.studyList.observe(viewLifecycleOwner,  {
+            Log.d("시발", "ㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ")
+            studyAdapter.submitList(it)
+        })
 
         binding.fabMyStudy.setOnClickListener {
             val intent = Intent(context, CreateStudy::class.java)
             startActivity(intent)
         }
-
-//        fetchStudy()
-//        val db = StudyDatabase.getInstance(requireContext())
-//        val studyList = db!!.getStudyDao().getAllStudy()
-//
-//        val tmpAdapter = TmpAdapter(studyList)
-//        binding.rvMyStudy.adapter = tmpAdapter
-
-//        CoroutineScope(Dispatchers.IO).launch {
-//            val db = StudyDatabase.getInstance(requireContext())
-//            var studyList = db!!.getStudyDao().getAllStudy()
-//
-//            val tmpAdapter = TmpAdapter(studyList)
-//            binding.rvMyStudy.adapter = tmpAdapter
-//        }
-
-
-
-
-//        val db = StudyDatabase.getInstance(requireContext())
-//        db!!.getStudyDao().getAllStudy().observe(viewLifecycleOwner, {
-//            binding.tvTmp.text = it.toString()
-//        })
-
-        var db = StudyDatabase.getInstance(requireContext())
-
-        db!!.getStudyDao().getAllStudy().observe(viewLifecycleOwner, {
-            val tmpAdapter = TmpAdapter(it)
-            Log.d("tmpAdapter", tmpAdapter.toString())
-            binding.rvMyStudy.adapter = tmpAdapter
-        })
 
         return binding.root
     }
@@ -78,22 +46,5 @@ class MyStudyFragment: Fragment() {
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
-    }
-
-    private fun adapterOnClick() {
-        val intent = Intent(context, ChatActivity::class.java)
-        startActivity(intent)
-    }
-
-    private fun fetchStudy() {
-        CoroutineScope(Dispatchers.IO).launch {
-            val db = StudyDatabase.getInstance(requireContext())
-            val studyList = db!!.getStudyDao().getAllStudy()
-
-//            val tmpAdapter = TmpAdapter(studyList)
-//            binding.rvMyStudy.adapter = tmpAdapter
-
-        }
-
     }
 }
