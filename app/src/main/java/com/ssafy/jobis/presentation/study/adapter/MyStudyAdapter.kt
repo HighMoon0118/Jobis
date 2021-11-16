c0647746fdbaee520892ec52c6420a2a670d9276
문학일 <mhi0118@naver.com>
2021-11-16 14:09:55 +0900

    ADD: 리사이클러뷰로 변경

--- app/src/main/java/com/ssafy/jobis/presentation/study/adapter/MyStudyAdapter.kt
+++ app/src/main/java/com/ssafy/jobis/presentation/study/adapter/MyStudyAdapter.kt
@@ -3,34 +3,29 @@ package com.ssafy.jobis.presentation.study.adapter
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.lifecycle.LiveData
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.ssafy.jobis.R
 import com.ssafy.jobis.data.model.study.Study
-import com.ssafy.jobis.presentation.study.viewholder.MyStudyHeaderViewHolder
 import com.ssafy.jobis.presentation.study.viewholder.MyStudyViewHolder
 
-class MyStudyAdapter(val context: Context): ListAdapter<Study, MyStudyViewHolder>(StudyDiffCallback) {
+class MyStudyAdapter(val context: Context, val studyList: List<Study>): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyStudyViewHolder {
         return MyStudyViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_my_study, parent, false), context)
     }
 
-    override fun onBindViewHolder(holder: MyStudyViewHolder, position: Int) {
-        val study = getItem(position)
-        holder.bind(study)
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val study = studyList[position]
+        if (holder is MyStudyViewHolder)
+            holder.bind(study)
     }
 
-}
-
-object StudyDiffCallback : DiffUtil.ItemCallback<Study>() {
-    override fun areItemsTheSame(oldItem: Study, newItem: Study): Boolean {
-        return oldItem == newItem
+    override fun getItemCount(): Int {
+        return  studyList.size
     }
 
-    override fun areContentsTheSame(oldItem: Study, newItem: Study): Boolean {
-        return oldItem.id == newItem.id
-    }
 }
