1ff8c4d8309a1a8fb53b6f02559cb0af78c32c40
Seongho <skk7541@gmail.com>
2021-11-17 01:59:08 +0900

    feat: 일정 추가 화면

--- app/src/main/java/com/ssafy/jobis/presentation/chat/ChatScheduleActivity.kt
+++ app/src/main/java/com/ssafy/jobis/presentation/chat/ChatScheduleActivity.kt
@@ -1,5 +1,6 @@
 package com.ssafy.jobis.presentation.chat
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -9,7 +10,9 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.google.android.gms.dynamic.SupportFragmentWrapper
 import com.google.firebase.firestore.FirebaseFirestore
+import com.ssafy.jobis.R
 import com.ssafy.jobis.data.model.calendar.Schedule
 import com.ssafy.jobis.data.response.JobResponse
 import com.ssafy.jobis.data.response.ScheduleResponse
@@ -43,6 +46,10 @@ class ChatScheduleActivity : AppCompatActivity() {
         })
 
         chatScheduleViewModel.loadScheduleList()
+
+        binding.addStudySchedule.setOnClickListener {
+            startActivity(Intent(this, ChatScheduleAddActivity::class.java))
+        }
     }
 
 
@@ -50,15 +57,15 @@ class ChatScheduleActivity : AppCompatActivity() {
         var schedules = ArrayList<Schedule>()
         for (i: Int in 0..scheduleList.size-1) {
             println("scheduleList[${i}]" + scheduleList[i].title)
-            println("scheduleList[${i}]" + scheduleList[i].studyId)
-            if (scheduleList[i].studyId != 0L) { // studyId가 있는 경우만
+            println("scheduleList[${i}]" + scheduleList[i].study_id)
+            if (scheduleList[i].study_id != 0L) { // studyId가 있는 경우만
                 var year = scheduleList[i].year.toInt()
                 var month = scheduleList[i].month.toInt()
                 var day = scheduleList[i].day.toInt()
                 var title = scheduleList[i].title
                 var content = scheduleList[i].content
-                var groupId = scheduleList[i].groupId.toInt()
-                var studyId = scheduleList[i].studyId.toInt()
+                var groupId = scheduleList[i].group_id.toInt()
+                var studyId = scheduleList[i].study_id.toInt()
                 var companyName = scheduleList[i].companyName
                 var startTime = scheduleList[i].start_time
                 var endTime = scheduleList[i].end_time
