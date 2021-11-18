2cb4843b7f1a16495d152d792ea3c67e940c9644
Seongho <skk7541@gmail.com>
2021-11-18 11:16:06 +0900

    feat: 스터디 일정 삭제

--- app/src/main/java/com/ssafy/jobis/presentation/chat/viewholder/ChatScheduleViewHolder.kt
+++ app/src/main/java/com/ssafy/jobis/presentation/chat/viewholder/ChatScheduleViewHolder.kt
@@ -1,18 +1,24 @@
 package com.ssafy.jobis.presentation.chat.viewholder
 
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.ssafy.jobis.R
 import com.ssafy.jobis.data.model.calendar.Schedule
+import com.ssafy.jobis.presentation.chat.ChatScheduleActivity
 
-class ChatScheduleViewHolder(view: View) : RecyclerView.ViewHolder(view){
+class ChatScheduleViewHolder(view: View, private val chatScheduleActivity: ChatScheduleActivity) : RecyclerView.ViewHolder(view){
     private val schedule_date : TextView = itemView.findViewById(R.id.study_schedule_date)
     private val schedule_title : TextView = itemView.findViewById(R.id.study_schedule_title)
     private val schedule_content : TextView = itemView.findViewById(R.id.study_schedule_content)
     private val schedule_time : TextView = itemView.findViewById(R.id.study_schedule_time)
+    private var study_delete_button: Button = itemView.findViewById(R.id.study_schedule_delete)
 
     fun bind(item: Schedule) {
+        study_delete_button.setOnClickListener {
+            chatScheduleActivity.onDeleteStudySchedule(item)
+        }
         var year = item.year
         var month = item.month
         var day = item.day
